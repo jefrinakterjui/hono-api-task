3783eb955b750d9116ebb3a29cb918a708545e4f
jefrinakterjui <jefrin.jui.dev@gmail.com>
2026-02-16 16:28:34 +0600

    setup openapi hono server and swagger ui

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { swaggerUI } from "@hono/swagger-ui";
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { logger } from "hono/logger";
+
+const app = new OpenAPIHono();
+
+app.use("*", logger())
+
+app.get("/", (c)=>{
+    return c.text("hono api is running!")
+})
+
+app.doc("/doc", {
+    openapi: "3.0.0",
+    info:{
+        version:"1.0.0",
+        title: "My Hono App",
+        description: "api  with hono and Drizzle"
+    }
+})
+
+app.get("/ui", swaggerUI({url: "/doc"}))
+
+export default app;
